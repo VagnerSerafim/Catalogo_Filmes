3d174512de9ac28ef6a5e5bd9518d1112b81e7fe
Vagner Serafim <vsvagenrserafim@gmail.com>
2025-08-07 21:51:32 -0300

    Commit Vagner feito Classe Filme - tem erros porquê ainda estamos trabalhando no projeto

---
+++ src/Filme.java
@@ -0,0 +1,80 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filme {
+
+    private String nome;
+    private String dataLancamento;
+    private BigDecimal orcamento;
+    private String descricao;
+    private Diretor diretor;
+    private List<Ator> atores;
+
+    public Filme(String nome, String dataLancamento, BigDecimal orcamento, String descricao) {
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+        this.orcamento = orcamento;
+        this.descricao = descricao;
+        this.atores = new ArrayList<>();
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public BigDecimal getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(BigDecimal orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public void adicionarAtor(Ator ator) {
+        this.atores.add(ator);
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de Lançamento: " + dataLancamento);
+        System.out.println("Orçamento: " + orcamento);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Diretor: " + (diretor != null ? diretor.getNome() : "Não definido"));
+        System.out.println("Atores: ");
+        for (Ator ator : atores) {
+            System.out.println("- " + ator.getNome());
+        }
+    }
+}
